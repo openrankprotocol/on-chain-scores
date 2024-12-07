730f42db9e80a16bbf17cbd49de0c5421de798dc
Eugene Kim <ek@karma3labs.com>
2024-12-06 17:07:15 -0800

    fix: do post-deployment test only for fresh deployments

--- script/OnChainScores.s.sol
+++ script/OnChainScores.s.sol
@@ -18,35 +18,36 @@ contract ComputeManagerScript is Script {
 
         if (proxy == 0x0000000000000000000000000000000000000000) {
             proxy = Upgrades.deployUUPSProxy("OnChainScoresV1.sol", abi.encodeCall(OnChainScoresV1.initialize, ()));
+            console.log("proxy deployed at:", proxy);
+
+            OnChainScoresV1 instance = OnChainScoresV1(proxy);
+            require(instance.healthCheck(1) == 42, "deployment health check failed");
+
+            users.push(OnChainScoresV1.User(2148, 100));
+            users.push(OnChainScoresV1.User(2147, 10));
+            instance.appendScores(users);
+
+            uint256 fid;
+            uint256 score;
+
+            require(instance.leaderboardLength() == 2);
+
+            (fid, score) = instance.leaderboard(0);
+            require(fid == 2148, "fid 2148 mismatch");
+            require(score == 100, "score mismatch for fid 2148");
+
+            (fid, score) = instance.leaderboard(1);
+
+            require(fid == 2147, "fid 2147 mismatch");
+            require(score == 10, "score mismatch for fid 2147");
+
+            instance.truncate(5);
+
+            require(instance.leaderboardLength() == 0);
         } else {
             Upgrades.upgradeProxy(proxy, "OnChainScoresV1.sol", "");
+            console.log("proxy deployed at:", proxy);
         }
-        console.log("proxy deployed at:", proxy);
-
-        OnChainScoresV1 instance = OnChainScoresV1(proxy);
-        require(instance.healthCheck(1) == 42, "deployment health check failed");
-
-        users.push(OnChainScoresV1.User(2148, 100));
-        users.push(OnChainScoresV1.User(2147, 10));
-        instance.appendScores(users);
-
-        uint256 fid;
-        uint256 score;
-
-        require(instance.leaderboardLength() == 2);
-
-        (fid, score) = instance.leaderboard(0);
-        require(fid == 2148, "fid 2148 mismatch");
-        require(score == 100, "score mismatch for fid 2148");
-
-        (fid, score) = instance.leaderboard(1);
-
-        require(fid == 2147, "fid 2147 mismatch");
-        require(score == 10, "score mismatch for fid 2147");
-
-        instance.truncate(5);
-
-        require(instance.leaderboardLength() == 0);
 
         vm.stopBroadcast();
     }
